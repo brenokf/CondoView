24d5d355b2f03bd10722a223ed9f6cf659300011
brenokf <breno-kf@hotmail.com>
2025-05-12 23:04:48 -0400

    Add Upload page to integrate UploadExcel component

---
+++ src/pages/upload.tsx
@@ -0,0 +1,9 @@
+import UploadExcel from "../components/UploadExcel";
+
+export default function Upload() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center p-4">
+      <UploadExcel />
+    </div>
+  );
+}
