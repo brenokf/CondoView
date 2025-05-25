d355acbea2264b5264c9f4c386513907eb7301d3
brenokf <breno-kf@hotmail.com>
2025-05-25 00:00:14 -0400

    Add Residents page with Table component integration

---
+++ src/pages/residents.tsx
@@ -0,0 +1,8 @@
+import Table from '../components/Table'
+export default function Residents() {
+  return (
+    <div className="min-h-screen bg-gray-100  items-center justify-center p-4">
+      < Table />
+    </div>
+  );
+}
