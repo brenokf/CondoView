015a0aa3b7eef79697777b4dcd9c254de44f15e5
brenokf <breno-kf@hotmail.com>
2025-05-25 23:05:51 -0400

    Add Detail page component with UserProfile integration

---
+++ src/pages/detail.tsx
@@ -0,0 +1,9 @@
+import UserProfile from "@/components/UserProfile";
+
+export default function Detail() {
+  return (
+    <div className="min-h-screen bg-gray-100  items-center justify-center p-4">
+      <UserProfile />
+    </div>
+  );
+}
