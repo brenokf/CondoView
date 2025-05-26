58473d40a2251ac66208c88aec47953e0966d095
brenokf <breno-kf@hotmail.com>
2025-05-25 23:06:19 -0400

    Refactor Residents page to use dynamic import for UserTable component

--- src/pages/residents.tsx
+++ src/pages/residents.tsx
@@ -1,8 +1,11 @@
-import Table from '../components/Table'
+import dynamic from "next/dynamic";
+
+const UserTablePage = dynamic(() => import("@/components/Table"), { ssr: false });
+
 export default function Residents() {
   return (
     <div className="min-h-screen bg-gray-100  items-center justify-center p-4">
-      < Table />
+      < UserTablePage />
     </div>
   );
 }
